b6b893c4e62d18955431923be3d4117fd99815a6
Gowthamigl <117185392+Gowthamigl@users.noreply.github.com>
2023-06-11 10:32:58 +0530

    Update database-connetion.js

--- config/database-connetion.js
+++ config/database-connetion.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-mongoose.connect("mongodb://127.0.0.1:27017/mywebsite").then(()=>{
+mongoose.connect("mongodb+srv://Gowthamigl:QRXYp8X7QHe8bRXG@gowthamcluster.tsfce1d.mongodb.net/?retryWrites=true&w=majority").then(()=>{
     console.log('data base connected')
 })
 .catch((err)=>{
